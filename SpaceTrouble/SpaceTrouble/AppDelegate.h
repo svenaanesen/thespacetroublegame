9148f8c4de285175db73ace1e2932942dd34db42
Sven Aanesen <sven.aanesen@gmail.com>
2012-09-07 15:55:51 +0200

    Initial project startup

---
+++ SpaceTrouble/SpaceTrouble/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SpaceTrouble
+//
+//  Created by Sven Aanesen on 07.09.12.
+//  Copyright (c) 2012 sven|way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SpaceTrouble/SpaceTrouble/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SpaceTrouble
+//
+//  Created by Sven Aanesen on 07.09.12.
+//  Copyright (c) 2012 sven|way. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
